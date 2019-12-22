cc425c21128f97468ba6bc3c6c3a081d9aaf3313
Loic Coenen <loic.coenen@email.com>
2019-12-22 19:33:32 +0000

    Broken

--- src/subscribe.rs
+++ src/subscribe.rs
@@ -1,7 +1,20 @@
 extern crate hyper;
 
 use hyper::{Body, Request, Response};
+use crate::hyper::rt::Stream;
+use std::str;
+use crate::hyper::rt::Future;
 
 pub fn handle(_req: Request<Body>) -> Response<Body> {
+    let (_parts, body) = _req.into_parts();
+    let chunks = body.collect().wait().unwrap(); 
+    let strings: Vec<String> = chunks.into_iter().map(|chunk| {
+
+        let buf: Vec<u8> = chunk.into_bytes().into_iter().collect();
+        String::from(str::from_utf8(&buf).unwrap())
+    }).collect();
+    let request_string = String::from(strings.concat());
+    print!("Request is {}", request_string);
+
     Response::new(Body::from("Subscribed"))
 }
