83df0ca1f1424bd075d4dbae9c5bfbec578adb00
Loic Coenen <loic.coenen@email.com>
2019-12-22 20:20:29 +0000

    :sparkles: Subscribing route implemented
    This implement the 'subscribe' route.
    
    - It accept an User object, with 3 properties (name, password, email)
    - It push it to `users` list on the redis server
    - It doesn't do any kind of modification
    - The server interpret TOML
    
    The commit also introduce a ./test.sh script

--- src/subscribe.rs
+++ src/subscribe.rs
@@ -4,17 +4,49 @@ use hyper::{Body, Request, Response};
 use crate::hyper::rt::Stream;
 use std::str;
 use crate::hyper::rt::Future;
+use std::thread;
 
-pub fn handle(_req: Request<Body>) -> Response<Body> {
+use serde::{Serialize, Deserialize};
+
+extern crate redis;
+use redis::Commands;
+use toml::ser::to_string;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    username: String,
+    password: String,
+    email: String
+}
+
+fn data(_req: Request<Body>) -> String {
     let (_parts, body) = _req.into_parts();
     let chunks = body.collect().wait().unwrap(); 
     let strings: Vec<String> = chunks.into_iter().map(|chunk| {
-
         let buf: Vec<u8> = chunk.into_bytes().into_iter().collect();
         String::from(str::from_utf8(&buf).unwrap())
     }).collect();
-    let request_string = String::from(strings.concat());
-    print!("Request is {}", request_string);
+    String::from(strings.concat())
+}
+
+pub fn handle(_req: Request<Body>) -> Response<Body> {
+    thread::spawn(|| {
+
+        let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+        let mut con = client.get_connection().unwrap();
+
+        let request_string = data(_req);
+        let user: User = toml::from_str(&request_string).unwrap();
+
+        println!("Request data is {}", request_string);
+        println!("username is {}", user.username);
+        println!("password is {}", user.password);
+        println!("email is {}", user.email);
+
+        let user_string = to_string(&user).unwrap();
+
+        let _ : () = con.lpush("users", user_string).unwrap();
+    });
 
     Response::new(Body::from("Subscribed"))
 }
---
+++ test.sh
@@ -0,0 +1,5 @@
+curl localhost:3000/subscribe -d '
+	username = "biter"
+	password = "jupiler"
+	email = "ijfedsa@gggg.com"
+'
